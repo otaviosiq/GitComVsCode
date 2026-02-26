89c990ed0daa563f32ff9e22d56d4efec5af882a
Otavio <otasiq@gmail.com>
2026-02-25 22:31:24 -0300

    Corrigindo eu quero ver como fica no Git

--- curso.py
+++ curso.py
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
-print("Ola Mundo Python Curso de Python")  
-
 print("Ola Mundo Python Curso de Python")
-=======
-print("Ola Mundo Python Curso de Python")
-
-print("Segunda Mundo Python Curso de Python")
->>>>>>> 971ee4ca81a646b15ec1ed0c904575121d18eea4
+print("Não é para ter mais porra de linha nenhuma!")
